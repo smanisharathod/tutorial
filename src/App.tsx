eb79eb8ebe13b9716f1089c5ea853c5e3792f51b
Shailesh Rathod <sainath@Shaileshs-Mac-mini.local>
2020-05-17 19:07:07 +0530

    creating a new section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import { Header, Footer } from './components';
+import React from "react";
+import { Header, Footer } from "./components";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <section className="ftco-section">
+        <div className="container">
+          <div className="row justify-content-center">
+          {/* ftco-animate */}
+            <div className="col-md-12 heading-section text-center mb-5"> 
+              <span className="subheading">Virtual Assistant Services</span>
+              <h2 className="mb-2">Our Services &amp; Offers</h2>
+            </div>
+          </div>
+        </div>
+        {/* <Service /> */}
+      </section>
       <Footer />
     </div>
   );
